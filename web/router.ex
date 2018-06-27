d6caf2b8e6b85604b34bb1b25eaafa584789ef28
Nicolas Arkkila <nicolasa@nicbook2.local>
2018-06-27 11:36:59 +0300

    Fix url

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule Chat.Router do
   scope "/", Chat do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/chat", PageController, :index
     post "/sms_relay", PageController, :sms_relay
   end
 
